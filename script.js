9067f4ba90aeedd817c79fc54306cc8bfb5d8d0e
Dianna Yim <dianna.yim@gmail.com>
2018-01-30 11:47:04 -0700

    asdf

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	"use strict";
+	
+	$("body").scrollspy({target: ".navbar", offset: 60});
+	
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 200) {
+			$("#navbar").removeClass("opaque");
+		}
+		else {
+			$("#navbar").addClass("opaque");
+		}
+	});
+	
+	$("#navbar").mouseenter(function() {
+		$("#navbar").addClass("opaque");
+	})
+	.mouseleave(function() {
+		if ($(window).scrollTop() > 200) {
+			$("#navbar").removeClass("opaque");
+		}
+	});
+	
+	$("#navbarSupportedContent a").on("click", function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			
+			var hash = this.hash;
+			
+			$("html, body").animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+				window.location.hash = hash;
+			});
+		}
+	});
+}); 
